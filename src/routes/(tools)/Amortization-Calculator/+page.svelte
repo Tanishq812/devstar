2ce04d38b179e6b6bdb4020b0e481582116333e3
Abhishek-2004-17 <abhisheksdaivathinveettil@gmail.com>
2023-09-30 22:07:34 +0530

    add

--- src/routes/(tools)/Amortization-Calculator/+page.svelte
+++ src/routes/(tools)/Amortization-Calculator/+page.svelte
@@ -1,12 +1,14 @@
 <script>
     let start=1000;
     let year = 15;
+    let interest = 5;
 </script>
 <h1>Amortization Calculator</h1>
 <form action="">
     Loan Amount<input type="number" class="sign" bind:value={start}>
     Loan Term<input type="number"  bind:value={year} placeholder="Year">
     <input type="number" placeholder="month">
+    Interest rate<input type="number" bind:value={interest}>
 </form>
 <style>
     h1{
